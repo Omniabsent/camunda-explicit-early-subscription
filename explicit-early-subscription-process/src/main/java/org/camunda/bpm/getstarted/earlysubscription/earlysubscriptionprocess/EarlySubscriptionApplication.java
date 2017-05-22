acf615a45188d2c5a36458f6ca8bc5fa5aa3dc59
Dennis Wolf <dennis.wolf0815@gmail.com>
2017-05-22 11:55:32 +0200

    remove: explicit early subscription process not supported anymore; functionality included in 'buffering-process'

--- explicit-early-subscription-process/src/main/java/org/camunda/bpm/getstarted/earlysubscription/earlysubscriptionprocess/EarlySubscriptionApplication.java
+++
@@ -1,9 +0,0 @@
-package org.camunda.bpm.getstarted.earlysubscription.earlysubscriptionprocess;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("Early Subscription Process")
-public class EarlySubscriptionApplication extends ServletProcessApplication {
-  // empty implementation
-}
