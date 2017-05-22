268d7f8f4ce83c7e6655d9c55d1c074256167e32
Dennis Wolf <dennis.wolf0815@gmail.com>
2017-05-22 22:15:51 +0200

    chore: remove unused code from TriggerBufferDeliveryDelegate

--- eventbuffer-util/src/main/java/org/hpi/bpt/eventbuffer/util/TriggerBufferDeliveryDelegate.java
+++ eventbuffer-util/src/main/java/org/hpi/bpt/eventbuffer/util/TriggerBufferDeliveryDelegate.java
@@ -17,7 +17,6 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 public class TriggerBufferDeliveryDelegate implements JavaDelegate {
 
 	private final static Logger LOGGER = Logger.getLogger("TRIGGER-BUFFER-DELIVERY");
-	private final static long TRIGGERDELAY_MS = 500L;
 
 	private Expression exp_messageName;
 
@@ -29,7 +28,7 @@ public class TriggerBufferDeliveryDelegate implements JavaDelegate {
 		String messageName = (String) exp_messageName.getValue(execution);
 
 		LOGGER.info("Delegate triggers event delivery for Process Definition " + processDefinitionId + ", messageName "
-				+ messageName + " with delay ms: " + TRIGGERDELAY_MS);
+				+ messageName);
 
 		// send start-event "buffering-task"
 		execution.getProcessEngineServices().getRuntimeService()
@@ -38,38 +37,6 @@ public class TriggerBufferDeliveryDelegate implements JavaDelegate {
 				.setVariable("event_messageName", messageName).setVariable("ref_processInstanceId", processInstanceId)
 				.correlateStartMessage();
 		LOGGER.info("Finished trigger buffer delivery.");
-
-		// TriggerEventDeliveryRunnable triggerRunnable = new
-		// TriggerEventDeliveryRunnable();
-		// triggerRunnable.execution = execution;
-		// triggerRunnable.messageName = messageName;
-		// triggerRunnable.processDefinitionId = processDefinitionId;
-		// triggerRunnable.processInstanceId = processInstanceId;
-		// new Thread(triggerRunnable).start();
 	}
 
-	public class TriggerEventDeliveryRunnable implements Runnable {
-		public String processInstanceId;
-		public String processDefinitionId;
-		public String messageName;
-
-		public DelegateExecution execution;
-
-		public void run() {
-			try {
-				Thread.sleep(TRIGGERDELAY_MS);
-			} catch (InterruptedException e) {
-				LOGGER.info("Couldn't sleep before triggering event delivery!");
-				e.printStackTrace();
-			}
-
-			execution.getProcessEngineServices().getRuntimeService()
-					.createMessageCorrelation("buffering-event-delivery-task")
-					.setVariable("event_processDefinitionId", processDefinitionId)
-					.setVariable("event_messageName", messageName)
-					.setVariable("ref_processInstanceId", processInstanceId).correlateStartMessage();
-			LOGGER.info("Finished trigger buffer delivery.");
-		}
-
-	}
 }
