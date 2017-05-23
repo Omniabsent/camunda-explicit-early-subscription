fa29dd6c7c9b49a380dd3f2111896c85a2c431da
Dennis Wolf <dennis.wolf0815@gmail.com>
2017-05-23 11:09:30 +0200

    refactor: moved eventbuffer-aux-processes into one maven project, renamed files and names accordingly, renamed folders

--- eventbuffer-delivery-process/src/main/java/org/hpi/bpt/eventbuffer/camundaprocesses/eventbufferdelivery/EventbufferDeliveryApplication.java
+++
@@ -1,9 +0,0 @@
-package org.hpi.bpt.eventbuffer.camundaprocesses.eventbufferdelivery;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("Eventbuffer Delivery App")
-public class EventbufferDeliveryApplication extends ServletProcessApplication {
-	// empty implementation
-}
--- example-generic-buffered-process/src/main/java/org/hpi/bpt/eventbuffer/camundaprocesses/bufferingprocess/BufferedProcessApplication.java
+++
@@ -1,9 +0,0 @@
-package org.hpi.bpt.eventbuffer.camundaprocesses.bufferingprocess;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("Example Buffered Process App")
-public class BufferedProcessApplication extends ServletProcessApplication {
-	// empty implementation
-}
